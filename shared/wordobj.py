c7079497a7c2f5c4809049e8d88228668e190609
Kai Maffucci <45264307+KaiMaffucci@users.noreply.github.com>
2026-08-13 12:13:22 -0400

    Cleaned up WordObj class

--- shared/wordobj.py
+++ shared/wordobj.py
@@ -5,9 +5,9 @@ Object which stores information about our words
 will be updated to contain other important data, but as of now:
 
 raw: raw text extracted from file/original text of word
-alt: an alternative spelling of original text
-trans: a transliteration of the word (perhaps to a different script)
+alt: an alternative spelling of original text (perhaps a different script)
 
+morphemes: parsed list of morphemes
 normal: normalized version of word
 ngrams: n-gram tokenization
 self.vec: vectorization of n-gram tokens
@@ -15,11 +15,10 @@ self.vec: vectorization of n-gram tokens
 
 class WordObj:
     
-    def __init__(self, raw="", alt="", trans="", normal=""):
+    def __init__(self, raw="", alt=""):
         self.raw = raw
         self.alt = alt
-        self.trans = trans
-        self.normal = normal
+        self.morphemes = []
+        self.normal = []
         self.ngrams = []
-        self.vec = []
-        #return self
+        self.vec = None
