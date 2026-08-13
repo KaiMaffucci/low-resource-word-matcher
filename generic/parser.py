df09dd5c8d3e6b8c79f6043ffeca95908050c6a3
Kai Maffucci <45264307+KaiMaffucci@users.noreply.github.com>
2026-08-12 20:58:15 -0400

    Implemented generic parser
    Pretty simple, just splits up each string into a list of chars.

--- generic/parser.py
+++ generic/parser.py
@@ -1,3 +1,19 @@
 
-def parse():
-    pass
+def parse(A_words, B_words):
+
+    # generic parser: each character is a morpheme (this is pretty terrible)
+    
+    for i in range(len(A_words)):
+        A_words[i].morphemes = list(A_words[i].raw)
+
+    for i in range(len(B_words)):
+        B_words[i].morphemes = list(B_words[i].raw)
+
+    # test code
+    for word in A_words:
+        print(word.morphemes)
+
+    for word in B_words:
+        print(word.morphemes)
+
+    return A_words, B_words
