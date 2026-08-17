ea112f61044e87e969d4948fd7b882f7787c9ecd
Kai Maffucci <45264307+KaiMaffucci@users.noreply.github.com>
2026-08-16 22:54:35 -0400

    Implemented Cherokee normalizer
    It's just the exact same one from the generic version. Nothing needs to change here.

--- cherokee/normalizer.py
+++ cherokee/normalizer.py
@@ -1,3 +1,14 @@
 
-def normalize():
-    pass
+def normalize(A_words, B_words):
+
+    import unicodedata
+
+    # NFC is good enough for our purposes
+    # this is a generic implementation, it's not meant to do anything fancy
+    for i in range(len(A_words)):
+        A_words[i].normal =  unicodedata.normalize('NFC', A_words[i].raw).strip().lower()
+
+    for i in range(len(B_words)):
+        B_words[i].normal =  unicodedata.normalize('NFC', B_words[i].raw).strip().lower()
+
+    return A_words, B_words
