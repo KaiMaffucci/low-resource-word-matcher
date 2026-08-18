55ab699910422125f87e5eab14f79162472332b1
Kai Maffucci <45264307+KaiMaffucci@users.noreply.github.com>
2026-08-18 09:10:53 -0400

    Continuing to work on n-gram Cherokee

--- cherokee/n_gram.py
+++ cherokee/n_gram.py
@@ -8,28 +8,11 @@ def n_gram(A_words, B_words, n=2):
     B_words = tokenize(B_words, n)
 
     # build ngram vocab for vectorization
-    # account for custom distances
-
-
-    # generate a vector for each word's n-gram list
-
-    """
-    1. If two morphemes start with the same consonant sound, they are 50% similar. Same with ending with vowels.
-    2. Consonant + vowel morphemes will count as 50% similar to standalone vowels if the vowels match; same logic for standalone consonants.
-    3. Consonant sounds ts = ch = j (distance between these is 0)
-    4. dl + vowel and tl + vowel should have a 25% difference for matching vowels, and 75% difference for nonmatching vowels, except for dla and tla, which should be 50% similar. Only the Ꮬ and Ꮭ syllables differentiate between dla and tla: all other tl syllables could also be dl.
-    5. Aspirated consonants + vowel (eg. hw + vowel), potentially barring hl + vowel (due to similarity with tl + vowel), will be considered 75% similar, or aspiration will be on a different axis entirely. h + consonant indicates aspiration, but is left out of syllabary and often left out of phonetics.
-    6. g and k (except when in ka) are considered 50% similar as opposed to 0%. This is to account for the syllables Ꭸ, Ꭹ, Ꭺ, Ꭻ, and Ꭼ all being transliterated as g + vowel in my program, even though they can also make the k sound. This needs to be accounted for since there is no prior word tagging.
-    """
-
-    # building the distance matrix is going to suck, but it'll work
-    import numpy as np
-    #distance_dict = {}
-
+    
     # build massive list of all morphemes based on my grammar
     """
     GRAMMAR (with or's after each line):
-    nah | hna | dla
+    nah | hna
     h consonant vowel
     consonant vowel
     ' vowel
@@ -62,35 +45,51 @@ def n_gram(A_words, B_words, n=2):
     all_morphemes = list(all_morphemes)
 
     # generate lists for custom distance rules
-    # note: are these even necessary? I'm gonna sleep on it and revisit it tomorrow
-    ts_sounds = [morph for morph in all_morphemes if morph[:-1] == "ts"]
-    j_sounds = [morph for morph in all_morphemes if morph[:-1] == "j"]
-    qu_sounds = [morph for morph in all_morphemes if morph[:-1] == "qu"]
-    tl_sounds = [morph for morph in all_morphemes if morph[:-1] == "tl"]
-    dl_sounds = [morph for morph in all_morphemes if morph[:-1] == "dl"]
-    d_sounds = [morph for morph in all_morphemes if morph[:-1] == "d"]
-    g_sounds = [morph for morph in all_morphemes if morph[:-1] == "g"]
-    k_sounds = [morph for morph in all_morphemes if morph[:-1] == "k"]
+    # add all syllables including standalone consonants/vowels
+    ts_sounds = [morph for morph in all_morphemes if morph[:-1] == "ts"].append("ts")
+    j_sounds = [morph for morph in all_morphemes if morph[:-1] == "j"].append("j")
+    qu_sounds = [morph for morph in all_morphemes if morph[:-1] == "qu"].append("qu")
+    tl_sounds = [morph for morph in all_morphemes if morph[:-1] == "tl"].append("tl")
+    dl_sounds = [morph for morph in all_morphemes if morph[:-1] == "dl"].append("dl")
+    d_sounds = [morph for morph in all_morphemes if morph[:-1] == "d"].append("d")
+    g_sounds = [morph for morph in all_morphemes if morph[:-1] == "g"].append("g")
+    k_sounds = [morph for morph in all_morphemes if morph[:-1] == "k"].append("k")
     # NOTE: *not* aspiration (h before consonant). This is for normal h syllables
-    h_sounds = [morph for morph in all_morphemes if morph[:-1] == "h"]
-    l_sounds = [morph for morph in all_morphemes if morph[:-1] == "l"]
+    h_sounds = [morph for morph in all_morphemes if morph[:-1] == "h"].append("h")
+    l_sounds = [morph for morph in all_morphemes if morph[:-1] == "l"].append("l")
+    s_sounds = [morph for morph in all_morphemes if morph[:-1] == "s"].append("s")
+    m_sounds = [morph for morph in all_morphemes if morph[:-1] == "m"].append("m")
+    n_sounds = [morph for morph in all_morphemes if morph[:-1] == "n"].append("n")
+    t_sounds = [morph for morph in all_morphemes if morph[:-1] == "t"].append("t")
+    w_sounds = [morph for morph in all_morphemes if morph[:-1] == "w"].append("w")
+    y_sounds = [morph for morph in all_morphemes if morph[:-1] == "y"].append("y")
 
+    # includes standalone vowels like a (for now)
+    a_sounds = [morph for morph in all_morphemes if morph[-1] == "a"]
+    e_sounds = [morph for morph in all_morphemes if morph[-1] == "e"]
+    i_sounds = [morph for morph in all_morphemes if morph[-1] == "i"]
+    o_sounds = [morph for morph in all_morphemes if morph[-1] == "o"]
+    u_sounds = [morph for morph in all_morphemes if morph[-1] == "u"]
+    v_sounds = [morph for morph in all_morphemes if morph[-1] == "v"]
 
-
-
-
-
-
-    a_sounds = []
-    e_sounds = []
-    o_sounds = []
+    # account for custom distances
     """
-    for m in all_morphemes:
-        if m[:-1] == "d":
-            d_sounds.append()
-    """
+    1. If two morphemes start with the same consonant sound, they are 50% similar. Same with ending with vowels.
+    2. Consonant + vowel morphemes will count as 50% similar to standalone vowels if the vowels match; same logic for standalone consonants.
+    3. Consonant sounds ts = ch = j (distance between these is 0)
+    4. dl + vowel and tl + vowel should have a 25% difference for matching vowels, and 75% difference for nonmatching vowels, except for dla and tla, which should be 50% similar. Only the Ꮬ and Ꮭ syllables differentiate between dla and tla: all other tl syllables could also be dl.
+    5. Aspirated consonants + vowel (eg. hw + vowel), potentially barring hl + vowel (due to similarity with tl + vowel), will be considered 75% similar, or aspiration will be on a different axis entirely. h + consonant indicates aspiration, but is left out of syllabary and often left out of phonetics.
+    6. g and k (except when in ka) are considered 50% similar as opposed to 0%. This is to account for the syllables Ꭸ, Ꭹ, Ꭺ, Ꭻ, and Ꭼ all being transliterated as g + vowel in my program, even though they can also make the k sound. This needs to be accounted for since there is no prior word tagging.
+    """    
+    distance_dict = {}
+    
 
 
+
+    # building the distance matrix is going to suck, but it'll work
+
+
+    # generate a vector for each word's n-gram list
 
 
     return A_words, B_words
