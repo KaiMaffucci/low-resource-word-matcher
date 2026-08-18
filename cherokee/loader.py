1223a29ea6b0560dd6858dd7f9b659db230ab9ac
Kai Maffucci <45264307+KaiMaffucci@users.noreply.github.com>
2026-08-17 20:26:54 -0400

    Implemented Cherokee syllabics transliteration
    We have two word sets, the control and the test. The test word set now contains the phonetic spelling of a word generated directly by transliterating the syllabics. There is no prior word tagging, so there will be some minor differences between these spellings and the original phonetics in the control. This is precisely what this project is about.

--- cherokee/loader.py
+++ cherokee/loader.py
@@ -14,15 +14,15 @@ def load(fname = input("Enter file name and path (my Anki deck): ")):
         r'^([A-Za-z\s,]*?)([\u13A0-\u13FF\uAB70-\uABBF\s,]*?)([A-Za-z\s,]*)$'
     )
 
-    """
-    where the syllabry spellings will go (later transliterated into phonetics)
-    raw text: syllabics
-    alt text: empty for now, will be transliterated into phonetics later
-    """
-    A_words = []
-    """where the original phonetic spellings will go (unchanged)
+    """where the original phonetic spellings will go (control)
     raw  text: syllabics
     alt text: original phonetic spelling
+    """
+    A_words = []
+    """
+    where the syllabry spellings will go (test, later transliterated into phonetics)
+    raw text: syllabics
+    alt text: empty for now, will be transliterated into phonetics later
     """
     B_words = []
 
@@ -41,13 +41,46 @@ def load(fname = input("Enter file name and path (my Anki deck): ")):
 
                 # if the syllabics has only one word, add it to word lists
                 if len(syllabics.split()) == 1:
-                    A_words.append(WordObj(syllabics))
-                    B_words.append(WordObj(syllabics, phonetics))
+                    A_words.append(WordObj(syllabics, phonetics))
+                    B_words.append(WordObj(syllabics))
 
-    # all said and done, print (test code)
-    for i in range(len(A_words)):
-        print(i)
-        print(f"{A_words[i].raw}\t{A_words[i].alt}")
-        print(f"{B_words[i].raw}\t{B_words[i].alt}")
+    # dictionary for transliterating syllabics into phonetics for test word list
+    # note: may move this to a json file at some point
+    translit_dict = {
+        # standalone vowels
+        "Ꭰ": "a", "Ꭱ": "e", "Ꭲ": "i", "Ꭳ": "o", "Ꭴ": "u", "Ꭵ": "v",
+        # G / K
+        "Ꭶ": "ga", "Ꭷ": "ka", "Ꭸ": "ge", "Ꭹ": "gi", "Ꭺ": "go", "Ꭻ": "gu", "Ꭼ": "gv",
+        # H
+        "Ꭽ": "ha", "Ꭾ": "he", "Ꭿ": "hi", "Ꮀ": "ho", "Ꮁ": "hu", "Ꮂ": "hv",
+        # L
+        "Ꮃ": "la", "Ꮄ": "le", "Ꮅ": "li", "Ꮆ": "lo", "Ꮇ": "lu", "Ꮈ": "lv",
+        # M
+        "Ꮉ": "ma", "Ꮊ": "me", "Ꮋ": "mi", "Ꮌ": "mo", "Ꮍ": "mu", "Ᏽ": "mv",
+        # N
+        "Ꮎ": "na", "Ꮏ": "hna", "Ꮑ": "ne", "Ꮐ": "nah", "Ꮒ": "ni", "Ꮓ": "no", "Ꮔ": "nu", "Ꮕ": "nv",
+        # QU
+        "Ꮖ": "qua", "Ꮗ": "que", "Ꮘ": "qui", "Ꮙ": "quo", "Ꮚ": "quu", "Ꮛ": "quv",
+        # S
+        "Ꮝ": "s", "Ꮜ": "sa", "Ꮞ": "se", "Ꮟ": "si", "Ꮠ": "so", "Ꮡ": "su", "Ꮢ": "sv",
+        # T / D
+        "Ꮣ": "da", "Ꮤ": "ta", "Ꮥ": "de", "Ꮦ": "te", "Ꮧ": "di", "Ꮨ": "ti", "Ꮩ": "do", "Ꮪ": "du", "Ꮫ": "dv",
+        # TL/DL
+        "Ꮬ": "dla", "Ꮭ": "tla", "Ꮮ": "tle", "Ꮯ": "tli", "Ꮰ": "tlo", "Ꮱ": "tlu", "Ꮲ": "tlv",
+        # TS (J)
+        "Ꮳ": "tsa", "Ꮴ": "tse", "Ꮵ": "tsi", "Ꮶ": "tso", "Ꮷ": "tsu", "Ꮸ": "tsv",
+        # W
+        "Ꮹ": "wa", "Ꮺ": "we", "Ꮻ": "wi", "Ꮼ": "wo", "Ꮽ": "wu", "Ꮾ": "wv",
+        # Y
+        "Ꮿ": "ya", "ye": "ye", "Ᏹ": "yi", "Ᏺ": "yo", "Ᏻ": "yu", "Ᏼ": "yv"
+    }
+
+    # perform actual transliteration
+    for word in B_words:
+        new_word = ""
+        for syllable in word.raw:
+            s = translit_dict.get(syllable, syllable)
+            new_word += s
+        word.alt = new_word
 
     return A_words, B_words
