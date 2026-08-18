08eabea6ea68766c17039fe25d1513a245d496ed
Kai Maffucci <45264307+KaiMaffucci@users.noreply.github.com>
2026-08-17 21:14:51 -0400

    Fixed generic parser
    Realized I never fixed the generic parser to use the normalized text when I switched the order of normalization and parsing (it now goes normalize, then parse).

--- generic/parser.py
+++ generic/parser.py
@@ -4,9 +4,9 @@ def parse(A_words, B_words):
     # generic parser: each character is a morpheme (this is pretty terrible)
     
     for i in range(len(A_words)):
-        A_words[i].morphemes = list(A_words[i].raw)
+        A_words[i].morphemes = list(A_words[i].normal)
 
     for i in range(len(B_words)):
-        B_words[i].morphemes = list(B_words[i].raw)
+        B_words[i].morphemes = list(B_words[i].normal)
 
     return A_words, B_words
