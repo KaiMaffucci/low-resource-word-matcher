458f628b84265cc8596dbfd7f9f6702955318bff
Kai Maffucci <45264307+KaiMaffucci@users.noreply.github.com>
2026-08-17 20:31:00 -0400

    Fixed Cherokee normalizer
    There were some things I had glossed over; these are now fixed.

--- cherokee/normalizer.py
+++ cherokee/normalizer.py
@@ -5,10 +5,11 @@ def normalize(A_words, B_words):
 
     # NFC is good enough for our purposes
     # this is a generic implementation, it's not meant to do anything fancy
+    # doing it on the alt text this time; that's where the phonetic (latin) spellings are stored
     for i in range(len(A_words)):
-        A_words[i].normal =  unicodedata.normalize('NFC', A_words[i].raw).strip().lower()
+        A_words[i].normal =  unicodedata.normalize('NFC', A_words[i].alt).strip().lower()
 
     for i in range(len(B_words)):
-        B_words[i].normal =  unicodedata.normalize('NFC', B_words[i].raw).strip().lower()
+        B_words[i].normal =  unicodedata.normalize('NFC', B_words[i].alt).strip() # already lowercase here
 
     return A_words, B_words
