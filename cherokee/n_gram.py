b4ac1f051c855db46cd3f42152e7ded4e6d1b532
Kai Maffucci <45264307+KaiMaffucci@users.noreply.github.com>
2026-08-18 11:09:51 -0400

    Update Cherokee n-grams
    There are a lot of annoying cases I have to account for. Working on it

--- cherokee/n_gram.py
+++ cherokee/n_gram.py
@@ -1,6 +1,5 @@
 
-#from shared.wordobj import WordObj
-
+# NOTE: "nah" syllable is kind of a problem?
 def n_gram(A_words, B_words, n=2):
 
     # generate n-grams for each word in each set
@@ -46,6 +45,7 @@ def n_gram(A_words, B_words, n=2):
 
     # generate lists for custom distance rules
     # add all syllables including standalone consonants/vowels
+    # TODO: maybe put this in a function?
     ts_sounds = [morph for morph in all_morphemes if morph[:-1] == "ts"].append("ts")
     j_sounds = [morph for morph in all_morphemes if morph[:-1] == "j"].append("j")
     qu_sounds = [morph for morph in all_morphemes if morph[:-1] == "qu"].append("qu")
@@ -78,13 +78,73 @@ def n_gram(A_words, B_words, n=2):
     2. Consonant + vowel morphemes will count as 50% similar to standalone vowels if the vowels match; same logic for standalone consonants.
     3. Consonant sounds ts = ch = j (distance between these is 0)
     4. dl + vowel and tl + vowel should have a 25% difference for matching vowels, and 75% difference for nonmatching vowels, except for dla and tla, which should be 50% similar. Only the Ꮬ and Ꮭ syllables differentiate between dla and tla: all other tl syllables could also be dl.
-    5. Aspirated consonants + vowel (eg. hw + vowel), potentially barring hl + vowel (due to similarity with tl + vowel), will be considered 75% similar, or aspiration will be on a different axis entirely. h + consonant indicates aspiration, but is left out of syllabary and often left out of phonetics.
+    5. Aspirated consonants + vowel (eg. hw + vowel) will be considered 75% similar, or aspiration will be on a different axis entirely. h + consonant indicates aspiration, but is left out of syllabary and often left out of phonetics.
     6. g and k (except when in ka) are considered 50% similar as opposed to 0%. This is to account for the syllables Ꭸ, Ꭹ, Ꭺ, Ꭻ, and Ꭼ all being transliterated as g + vowel in my program, even though they can also make the k sound. This needs to be accounted for since there is no prior word tagging.
-    """    
-    distance_dict = {}
+    """
+
+    # this function returns a dictionary to update the main big dictionary with
+    def add_dists(sounds1, sounds2=[], dist=0.5):
+        to_add = {}
+        if sounds2 == []:
+            sounds2 = sounds1
+        for m1 in sounds1:
+            for m2 in sounds2:
+                if m1 != m2:
+                    to_add[frozenset(m1, m2)] = dist
+        return to_add
+
+    # NOTE: PROBLEM: need to account for situations like ge and ke that have both a matching vowel and semi-matching consonant, etc
+    # def add_edge_cases()
+
+    dist_dict = {}
+
+    # morphemes with shared consonants (including standalone) have 0.5 distance, except dla (has own character in syllabary)
+    # this will get some redundant ones like k- and g- that we will fix later
+    dist_dict.update(add_dists(ts_sounds))
+    dist_dict.update(add_dists(j_sounds))
+    dist_dict.update(add_dists(qu_sounds))
+    dist_dict.update(add_dists(tl_sounds))
+    dist_dict.update(add_dists(d_sounds))
+    dist_dict.update(add_dists(dl_sounds))
+    dist_dict.update(add_dists(g_sounds))
+    dist_dict.update(add_dists(k_sounds))
+    dist_dict.update(add_dists(h_sounds))
+    dist_dict.update(add_dists(l_sounds))
+    dist_dict.update(add_dists(m_sounds))
+    dist_dict.update(add_dists(n_sounds))
+    dist_dict.update(add_dists(s_sounds))
+    dist_dict.update(add_dists(t_sounds))
+    dist_dict.update(add_dists(w_sounds))
+    dist_dict.update(add_dists(y_sounds))
+
+    # morphemes with shared vowels (including standalone) have 0.5 distance
+    dist_dict.update(add_dists(a_sounds))
+    dist_dict.update(add_dists(e_sounds))
+    dist_dict.update(add_dists(i_sounds))
+    dist_dict.update(add_dists(o_sounds))
+    dist_dict.update(add_dists(u_sounds))
+    dist_dict.update(add_dists(v_sounds))
+
+    # ts = j => 0 distance (may need to add ch later)
     
 
+    # dl + tl (except dla and tla) have 0.25 distance for matching vowel morphemes, nonmatching vowels have 0.75 distance
+    dist_dict.update(add_dists(tl_sounds, dl_sounds, 0.75))
 
+    dist_dict[frozenset("dla", "tla")] = 0.5
+
+    # (h consonant [vowel]) have distance of 0.25 from original matching (consonant vowel)
+
+
+    # g and k syllables (except in "ka" and "ga" because they have different characters)
+    # have 0.25 distance, like dl- and tl-
+
+
+    # test code: print dictionary
+
+
+    # TODO: save this to a json file and add option for loading it
+    # so we don't have to generate all this every time
 
     # building the distance matrix is going to suck, but it'll work
 
